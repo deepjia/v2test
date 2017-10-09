38670bc0564d00e9b6480bd28a641b1c1738d6cc
deepjia <deepjia@gmail.com>
2017-10-09 13:31:10 +0800

    Fix Shell engine.

--- TestEngines/shell.py
+++ TestEngines/shell.py
@@ -11,27 +11,36 @@ class Test:
 
     @staticmethod
     def action(action_value, action):
+        shell = None
         if action =='python':
             path = CONFIG.get('SHELL','PYTHON')
             path = path if path else sys.executable
-            action_value = [path, os.path.join(FILE_DIR, action_value)]
+            path = path.split()
+            path.append(os.path.join(FILE_DIR, action_value))
         elif action =='bash':
             path = CONFIG.get('SHELL', 'BASH')
             path = path if path else '/usr/bin/bash'
-            action_value = [path, os.path.join(FILE_DIR, action_value)]
+            path = path.split()
+            path.append(os.path.join(FILE_DIR, action_value))
         elif action == 'ruby':
             path = CONFIG.get('SHELL', 'RUBY')
             path = path if path else '/usr/bin/ruby'
-            action_value = [path, os.path.join(FILE_DIR, action_value)]
+            path = path.split()
+            path.append(os.path.join(FILE_DIR, action_value))
         elif action == 'perl':
             path = CONFIG.get('SHELL', 'PERL')
             path = path if path else '/usr/bin/perl'
-            action_value = [path, os.path.join(FILE_DIR, action_value)]
+            path = path.split()
+            path.append(os.path.join(FILE_DIR, action_value))
         elif action == 'file':
-            action_value = os.path.join(FILE_DIR, action_value)
-        elif action != 'command':
+            path = os.path.join(FILE_DIR, action_value)
+            shell = True
+        elif action == 'command':
+            path = action_value
+            shell = True
+        else:
             raise ValueError('Invalid Action.')
-        r = subprocess.run(action_value, shell=True, check=True, stdout=subprocess.PIPE)
+        r = subprocess.run(path, shell=shell, check=True, stdout=subprocess.PIPE)
         return r.stdout.decode('utf-8')
 
     @staticmethod
