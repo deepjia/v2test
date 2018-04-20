ec77d5fb17e5059a7dbee26e9509ae24ee756720
deepjia <deepjia@gmail.com>
2018-04-20 11:32:22 +0800

    Add 404 and 500 page

--- v2testmgr/v2testmgr.py
+++ v2testmgr/v2testmgr.py
@@ -295,5 +295,15 @@ def run():
     return json.dumps(res)
 
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
+
+
 if __name__ == '__main__':
     app.run()
